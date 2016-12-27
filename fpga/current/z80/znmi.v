9aeb7abef001973d8fa36507904b2e2c33544a4f
lvd <lvd@b4c23ef4-51d0-483d-aa81-0a5320716916>
2016-12-27 20:07:05 +0000

    git-svn-id: svn://localhost/nedopc/pentevo@713 b4c23ef4-51d0-483d-aa81-0a5320716916

--- fpga/current/z80/znmi.v
+++ fpga/current/z80/znmi.v
@@ -1,4 +1,4 @@
-// ZX-Evo Base Configuration (c) NedoPC 2008,2009,2010,2011,2012,2013,2014
+// ZX-Evo Base Configuration (c) NedoPC 2008,2009,2010,2011,2012,2013,2014,2015,2016
 //
 // NMI generation
 
