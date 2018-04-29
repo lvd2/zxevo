8af7faf509386642666f1a41b649482d159b5dce
savelij <savelij@b4c23ef4-51d0-483d-aa81-0a5320716916>
2018-04-29 09:42:10 +0000

    ERS v0.57f не отображалось имя FDI образа загруженного в рамдиск
    git-svn-id: svn://localhost/nedopc/pentevo@780 b4c23ef4-51d0-483d-aa81-0a5320716916

--- rom/page0/Source/make_main.bat
+++ rom/page0/Source/make_main.bat
@@ -1,6 +1,6 @@
 @ECHO OFF
 
-..\..\..\tools\asw\bin\asw -U -L -D DELVAR=0 main.a80
+..\..\..\tools\asw\bin\asw -U -L -D DOS_FE=0 main.a80
 ..\..\..\tools\asw\bin\p2bin main.p main.rom -r $-$ -k
 
 ..\..\..\tools\mhmt\mhmt -mlz main.rom main_pack.rom
