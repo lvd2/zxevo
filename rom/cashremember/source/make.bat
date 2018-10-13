24885599ceb9e2ae575cc13e96475513c47a3c18
savelij <savelij@b4c23ef4-51d0-483d-aa81-0a5320716916>
2018-10-13 18:41:26 +0000

    Версия 0.57g Заменен драйвер HDD
    git-svn-id: svn://localhost/nedopc/pentevo@781 b4c23ef4-51d0-483d-aa81-0a5320716916

--- rom/cashremember/source/make.bat
+++ rom/cashremember/source/make.bat
@@ -1,15 +1,15 @@
 
 @ECHO OFF
 
-asw -U -L cash20.a80
-p2bin cash20.p cash20.rom -r $-$ -k
+..\..\..\tools\asw\bin\asw -U -L cash20.a80
+..\..\..\tools\asw\bin\p2bin cash20.p cash20.rom -r $-$ -k
 
 rem asw -U -L viewer.a80
 rem p2bin viewer.p viewer.rom -r $-$ -k
 
-mhmt -mlz cash20.rom ..\cashrm_pack.rom
-mhmt -mlz sts4_3j.bin ..\sts43j_pack.rom
-mhmt -mlz sts4_3i.bin ..\sts43i_pack.rom
+..\..\..\tools\mhmt\mhmt -mlz cash20.rom ..\cashrm_pack.rom
+..\..\..\tools\mhmt\mhmt -mlz sts4_3j.bin ..\sts43j_pack.rom
+..\..\..\tools\mhmt\mhmt -mlz sts4_3i.bin ..\sts43i_pack.rom
 
 rem asw -U -L make_scl.a80
 rem p2bin make_scl.p make_scl.rom -r $-$ -k
