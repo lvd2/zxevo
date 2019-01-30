55a252cb4d94392675c61486019a03b24690567c
savelij <savelij@b4c23ef4-51d0-483d-aa81-0a5320716916>
2019-01-30 20:18:03 +0000

    Залил некоторые пропущеные файлы.
    git-svn-id: svn://localhost/nedopc/pentevo@802 b4c23ef4-51d0-483d-aa81-0a5320716916

---
+++ rom/page1/dos_fe/make.bat
@@ -0,0 +1,9 @@
+
+@ECHO OFF
+
+..\..\..\tools\asw\bin\asw -U -L dos_fe.a80
+..\..\..\tools\asw\bin\p2bin dos_fe.p dos_fe.rom -r $-$ -k
+
+..\..\..\tools\mhmt\mhmt -mlz dos_fe.rom ..\dos_fe_pack.rom
+
+REM del dos_fe.rom
