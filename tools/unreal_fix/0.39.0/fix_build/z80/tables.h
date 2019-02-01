a822476c0f52a476d760e70e1244af8f84427ced
DimkaM <DimkaM@b4c23ef4-51d0-483d-aa81-0a5320716916>
2019-02-01 15:30:34 +0000

    unreal: fix makefile
    git-svn-id: svn://localhost/nedopc/pentevo@805 b4c23ef4-51d0-483d-aa81-0a5320716916

--- tools/unreal_fix/0.39.0/fix_build/z80/tables.h
+++ tools/unreal_fix/0.39.0/fix_build/z80/tables.h
@@ -5,24 +5,89 @@ extern const unsigned char rl1[];
 extern const unsigned char rr0[];
 extern const unsigned char rr1[];
 
-extern unsigned char rol[];
-extern unsigned char ror[];
-
-extern unsigned char rlcaf[];
-extern unsigned char rrcaf[];
 extern const unsigned char rlcf[];
 extern const unsigned char rrcf[];
 
+#ifdef __ICL
+extern unsigned char rol[];
+extern unsigned char ror[];
+extern unsigned char rlcaf[];
+extern unsigned char rrcaf[];
+#else
+struct TMakeRot
+{
+    constexpr TMakeRot();
+
+    u8 rol[0x100];
+    u8 ror[0x100];
+    u8 rlcaf[0x100];
+    u8 rrcaf[0x100];
+};
+
+extern const TMakeRot MakeRot;
+#define rol MakeRot.rol
+#define ror MakeRot.ror
+#define rlcaf MakeRot.rlcaf
+#define rrcaf MakeRot.rrcaf
+#endif
+
 extern const unsigned char sraf[];
+
+
+#ifdef __ICL
 extern unsigned char log_f[];
+#else
+struct TLogf
+{
+    constexpr TLogf();
+
+    u8 log_f[0x100];
+};
+
+extern const TLogf Logf;
+#define log_f Logf.log_f
+#endif
 
 extern const unsigned char incf[];
 extern const unsigned char decf[];
-extern unsigned char adcf[];
-extern unsigned char sbcf[];
 
+#ifdef __ICL
+extern unsigned char adcf[];
+#else
+
+struct TMakeAdcf
+{
+    constexpr TMakeAdcf();
+
+    u8 adcf[0x20000];
+};
+
+extern const TMakeAdcf MakeAdcf;
+
+#define adcf MakeAdcf.adcf
+#endif
+
+#ifdef __ICL
+extern unsigned char sbcf[];
 extern unsigned char cpf[];
 extern unsigned char cpf8b[];
+#else
+
+struct TMakeSbc
+{
+    constexpr TMakeSbc();
+
+    u8 sbcf[0x20000];
+    u8 cpf[0x10000];
+    u8 cpf8b[0x10000];
+};
+
+extern const TMakeSbc MakeSbc;
+
+#define sbcf MakeSbc.sbcf
+#define cpf MakeSbc.cpf
+#define cpf8b MakeSbc.cpf8b
+#endif
 
 Z80INLINE void and8(Z80 *cpu, unsigned char src)
 {
