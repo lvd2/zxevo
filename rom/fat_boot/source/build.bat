9f2f92a5bf1036cfbd845f6d0e76b208d7e6b22d
savelij <savelij@b4c23ef4-51d0-483d-aa81-0a5320716916>
2021-08-01 17:11:54 +0000

    - добавлено сохранение/восстановление порта виртуальных дисководов для RST 8, NMI. - переписана работа с FAT. - поправлены запускалки форматов Hobeta, SNA, SPG
    git-svn-id: svn://localhost/nedopc/pentevo@961 b4c23ef4-51d0-483d-aa81-0a5320716916

--- rom/fat_boot/source/build.bat
+++ rom/fat_boot/source/build.bat
@@ -2,8 +2,6 @@
 echo off
 
 ..\..\..\tools\asw\bin\asw -U -L -x -D DELVAR=0 micro_boot_fat.a80
-..\..\..\tools\asw\bin\p2bin micro_boot_fat.p micro_boot_fat.rom -r $-$ -k
+..\..\..\tools\asw\bin\p2bin micro_boot_fat.p ..\micro_boot_fat.rom -r $-$ -k
 
-..\..\..\tools\mhmt\mhmt -mlz micro_boot_fat.rom ..\micro_boot_fat_pack.rom
-
-del micro_boot_fat.rom
+..\..\..\tools\mhmt\mhmt -mlz ..\micro_boot_fat.rom ..\micro_boot_fat_pack.rom
--- rom/page5/source/build.bat
+++ rom/page5/source/build.bat
@@ -1,5 +1,5 @@
 
-echo off
+echo on
 
 cd ..\..\fat_boot\source
 
