bb68f0f4469a74c39c529efc8068a5a3ee9065b0
alone <alone@b4c23ef4-51d0-483d-aa81-0a5320716916>
2021-11-06 09:22:02 +0000

    fix PATH (DimkaM)
    git-svn-id: svn://localhost/nedopc/pentevo@974 b4c23ef4-51d0-483d-aa81-0a5320716916

--- cfgs/standalone_base_trdemu/trunk/winbuild.bat
+++ cfgs/standalone_base_trdemu/trunk/winbuild.bat
@@ -1,6 +1,7 @@
 @ECHO OFF
 rem set PATH=c:\mingw\avrgcc\bin;%PATH%
-set PATH=c:\mingw\avrgcc\bin;c:\mingw\bin;c:\mingw\msys\bin
+rem set PATH=c:\mingw\avrgcc\bin;c:\mingw\bin;c:\mingw\msys\bin
+set PATH=c:\mingw\avrgcc\bin;C:\Windows\system32;C:\Windows;C:\Windows\SysWOW64
 
 set FPGA_PATH=fpga/quartus
 set FPGA_FILE=top.rbf
@@ -23,5 +24,5 @@ IF NOT EXIST %FPGA_PATH%/%FPGA_FILE% (
 %MHMT_BIN% -maxwin2048 %FPGA_PATH%/%FPGA_FILE% %AVR_PATH%/%AVR_FILE%
 
 make -C %AVR_PATH%
-copy avr\build\zxevo.bin zxevo_fw.bin
+rem copy avr\build\zxevo.bin zxevo_fw.bin
 copy zxevo_fw.bin e:\zxevo_fw.bin
