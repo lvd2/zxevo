e6bc8b9744d2ebcbe13b9f18db779b10b6a191c9
savelij <savelij@b4c23ef4-51d0-483d-aa81-0a5320716916>
2021-11-22 15:46:09 +0000

    При не соответствии прошивки ROM zxevo_fe.rom и FPGA на экран выводится «Incorrect FPGA zxevo_fw.bin» на красном фоне. Запрос версии через RST 8: _VERSION в «A» возвращает для какой сборки собран ROM файл и соответствие прошивке FPGA.
    git-svn-id: svn://localhost/nedopc/pentevo@977 b4c23ef4-51d0-483d-aa81-0a5320716916

--- rom/build_full.sh
+++ rom/build_full.sh
@@ -1,97 +1,65 @@
 #!/bin/bash
 
-echo ####################
-echo ## BUILD FAT BOOT ##
-echo ####################
+# BUILD FAT BOOT
 cd fat_boot/source
 ./build.sh
 
-echo #####################
-echo ## BUILD MAIN MENU ##
-echo #####################
+# BUILD MAIN MENU
 cd ../../mainmenu/src
 ./build.sh
 
-echo ##################
-echo ## BUILD DOS_FE ##
-echo ##################
+# BUILD DOS_FE
 cd ../../page1/dos_fe
 ./build.sh
 
-echo #######################
-echo ## BUILD TR-DOS 5.03 ##
-echo #######################
+# BUILD TR-DOS 5.03
 cd ../../page1/trdos503
 ./build.sh
 
-echo ######################
-echo ## BUILD START PAGE ##
-echo ######################
+# BUILD START PAGE
 cd ../../page0/source
 ./build.sh
 
-echo ###################
-echo ## BUILD EVO-DOS ##
-echo ###################
+# BUILD EVO-DOS
 cd ../../page1/evo-dos
 ./build.sh
 
-echo #####################
-echo ## BUILD BASIC 128 ##
-echo #####################
+# BUILD BASIC 128
 cd ../../page2/source
 ./build.sh
 
-echo ####################
-echo ## BUILD BASIC 48 ##
-echo ####################
+# BUILD BASIC 48
 cd ../../page3/source
 ./build.sh
 
-echo ####################
-echo ## BUILD ATM CP/M ##
-echo ####################
+# BUILD ATM CP/M
 cd ../../atm_cpm/source
 ./build.sh
 
-echo ##########################
-echo ## BUILD RST 8 SERVICES ##
-echo ##########################
+# BUILD RST 8 SERVICES
 cd ../../page5/source
 ./build.sh
 
-echo #######################
-echo ## BUILD TR-DOS 6.10 ##
-echo #######################
-ccd ../../trdos_v6/source
+# BUILD TR-DOS 6.10 
+cd ../../trdos_v6/source
 ./build.sh
 
 cd ../..
 
-echo ###############
-echo ## BUILD ERS ##
-echo ###############
+# BUILD ERS 
 cat page3/basic48_128.rom page1/evo-dos_virt.rom page5/rst8service.rom    ff_16k.rom page3/basic48_128.rom page1/evo-dos_emu3d13.rom page2/basic128.rom page0/services.rom    > ers.rom
 cat ff_16k.rom            ff_16k.rom             page5/rst8service_fe.rom ff_16k.rom page3/basic48_128.rom page1/tr5_03.rom          page2/basic128.rom page0/services_fe.rom > ers_fe.rom
 
-echo #####################
-echo ## BUILD PENT GLUK ##
-echo #####################
+# BUILD PENT GLUK 
 cat page3/2006.rom trdos_v6/dosatm3.rom page2/basic128.rom page0/glukpen.rom > glukpent.rom
 
-echo ####################
-echo ## BUILD ATM CP/M ##
-echo ####################
+# BUILD ATM CP/M 
 cat atm_cpm/rbios.rom page3/basic48_128_std.rom page2/128_std.rom page3/basic48_orig.rom > basics_std.rom
 
-echo ########################
-echo ## BUILD FULL ERS ROM ##
-echo ########################
+# BUILD FULL ERS ROM
 cat ff_64k.rom basics_std.rom glukpent.rom profrom/evoprofrom.rom ers.rom > zxevo.rom
 
-echo ####################################
-echo ## BUILD FULL ERS ROM EMUL FDD FE ##
-echo ####################################
+# BUILD FULL ERS ROM EMUL FDD FE
 #rem    64          64            64                128               192
 cat ff_64k.rom basics_std.rom glukpent.rom profrom/evoprofrom.rom ers_fe.rom > zxevo_fe.rom
 
@@ -100,4 +68,4 @@ rm ers_fe.rom
 rm glukpent.rom
 rm basics_std.rom
 
-echo; read -rsn1 -p "Press any key to continue . . .";
+read -rsn1 -p "Press any key to continue . . .";
